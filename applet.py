4d18058b6005cee1a2bc4fb6ecb88dc7f716f2f4
Alban Q <37601993+albanqafa@users.noreply.github.com>
2023-02-03 03:07:35 -0500

    Update applet.py

--- applet.py
+++ applet.py
@@ -4,40 +4,77 @@ gi.require_version('XApp', '1.0')
 from gi.repository import Gtk, XApp
 import requests
 
+host = "192.168.1.121"
+port = "8123"
+endpoint = lambda a: "http://" + host + ":" + port + a
+rgb_luminence = "/api/services/light/turn_on"
+light_toggle = "/api/services/light/toggle"
+
+ha_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxNzVmZDJlYWZmODA0OGJmOTk4ZDE5MTJhMGQ3ZDkwZCIsImlhdCI6MTY3NTMzMjkwOSwiZXhwIjoxOTkwNjkyOTA5fQ.NfPsebbQl28eEKSSOHciA9ThJ_UlJXt45s6H-ZNrwrE"
+
+print(endpoint(rgb_luminence))
+print(endpoint(light_toggle))
+
 direction = ""
+data = {}
 headers = {
 	"Content-Type": "application/json",
-	"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxNzVmZDJlYWZmODA0OGJmOTk4ZDE5MTJhMGQ3ZDkwZCIsImlhdCI6MTY3NTMzMjkwOSwiZXhwIjoxOTkwNjkyOTA5fQ.NfPsebbQl28eEKSSOHciA9ThJ_UlJXt45s6H-ZNrwrE"
+	"Authorization": "Bearer " + ha_token
 }
 
-def on_scroll_event(status_icon, event, x, y):
-	print(status_icon.props.name)
-	icon_number = status_icon.props.name
-	if event == 1:
-		direction = "down"
-	elif event == -1:
-		direction = "up"
-	print(direction)
-	bright_dim(direction, icon_number)
+def api_send(endpoint, post_data):
+	response = requests.post(endpoint, headers=headers, json=post_data)
+	print(response)
 
-def bright_dim(direction, icon_number):
-	url = "http://192.168.1.121:8123/api/services/light/turn_on"
-	if direction == "up":
-		data = {
-			"brightness_step": "20"
-		}
-	elif direction == "down":
-			data = {
-			"brightness_step": "-20"
-		}
+
+def data_select(icon_number, direction="", rgb_color=[]):
+	data = {}
 	if icon_number == "icon1":
 		data.update({"entity_id": "light.office_lights"})
 	elif icon_number == "icon2":
 		data.update({"entity_id": "light.living_room_lights"})
 	elif icon_number == "icon3":
 		data.update({"entity_id": "light.front_room_lights"})
-	response = requests.post(url, headers=headers, json=data)
-	print(response)
+
+	if direction != "":
+		if direction == "up":
+			data.update({"brightness_step": "20"})
+		elif direction == "down":
+			data.update({"brightness_step": "-20"})
+
+	if rgb_color != []:
+		data.update({"rgb_color": rgb_color})
+		
+	print(data)
+	return data
+	del data
+
+
+def on_button_event(status_icon, button, time):
+	icon_number = status_icon.props.name
+	if button == 1: #left click
+		change_toggle(icon_number)
+	elif button == 3: #right click
+		change_color(icon_number)
+
+
+def on_scroll_event(status_icon, event, x, y):
+	icon_number = status_icon.props.name
+	if event == 1:
+		direction = "down"
+	elif event == -1:
+		direction = "up"
+	change_luminence(icon_number, direction)
+	del direction
+
+
+def change_toggle(icon_number):
+	api_send(endpoint(light_toggle), data_select(icon_number))
+
+
+def change_luminence(icon_number, direction):
+	api_send(endpoint(rgb_luminence), data_select(icon_number, direction))
+
 
 def change_color(icon_number):
 	color_chooser = Gtk.ColorChooserDialog("Choose a color")
@@ -54,38 +91,9 @@ def change_color(icon_number):
 			red, green, blue, alpha
 		))
 	color_chooser.destroy()
-	url = "http://192.168.1.121:8123/api/services/light/turn_on"
-	data = {
-	"rgb_color": [red,green,blue]
-	}
-	if icon_number == "icon1":
-		data.update({"entity_id": "light.office_lights"})
-	elif icon_number == "icon2":
-		data.update({"entity_id": "light.living_room_lights"})
-	elif icon_number == "icon3":
-		data.update({"entity_id": "light.front_room_lights"})
-	response = requests.post(url, headers=headers, json=data)
-	print(response)
+	api_send(endpoint(rgb_luminence), data_select(icon_number, None, [red,green,blue]))
 
-def toggle(icon_number):
-	url = "http://192.168.1.121:8123/api/services/light/toggle"
-	if icon_number == "icon1":
-		data = {"entity_id": "light.office_lights"}
-	elif icon_number == "icon2":
-		data = {"entity_id": "light.living_room_lights"}
-	elif icon_number == "icon3":
-		data = {"entity_id": "light.front_room_lights"}
-	response = requests.post(url, headers=headers, json=data)
-	print(response)
 
-def on_button_event(status_icon, button, time):
-	print(status_icon.props.name)
-	print(button)
-	icon_number = status_icon.props.name
-	if button == 1:
-		toggle(icon_number)
-	elif button == 3:
-		change_color(icon_number)
 
 icon1 = XApp.StatusIcon()
 icon1.connect("scroll-event", on_scroll_event)
